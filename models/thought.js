bd3f91fd1c611b9ef9a7d093676a8ed338a5194f
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-06-01 01:20:47 -0400

    made some changes to thought.js model

--- models/thought.js
+++ models/thought.js
@@ -1,32 +1,5 @@
 const mongoose = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: { 
-        type: String, 
-        required: true,
-        minlength: 1,
-        maxlength: 280
-        },
-    createdAt: { 
-        type: Date,
-        default: Date.now
-        },
-    username: { 
-        type: String, 
-        ref: 'Thought' 
-        },
-    reactions: [ 
-        reactionSchema 
-        ]
-},
-{
-    toJSON: {
-            virtuals: true,
-            getters: true,
-            },
-    id: false,
-});
-
 const reactionSchema = new mongoose.Schema({
     reactionId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -53,6 +26,33 @@ const reactionSchema = new mongoose.Schema({
       id: false,
     }
 );
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: { 
+        type: String, 
+        required: true,
+        minlength: 1,
+        maxlength: 280
+        },
+    createdAt: { 
+        type: Date,
+        default: Date.now
+        },
+    username: { 
+        type: String, 
+        ref: 'Thought' 
+        },
+    reactions: [ 
+        reactionSchema 
+        ]
+},
+{
+    toJSON: {
+            virtuals: true,
+            getters: true,
+            },
+    id: false,
+});
 
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
